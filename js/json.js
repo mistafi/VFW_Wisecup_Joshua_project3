b5bc85c0b0f53cc0190bc9355bd352c393bc6766
mistafi <mistafi@gmail.com>
2013-01-29 23:18:25 -0500

    building json

---
+++ js/json.js
@@ -0,0 +1,37 @@
+// JSON Document
+// VFW Project 3 
+// Joshua Wisecup
+// Term 0113 
+
+
+var jsonData = {
+	"pebble1": {
+		"dropdownSelect": ["Type:", "Restaurant"],
+		"inputName": ["Name:", "Johnny's Pizza"],	
+		"inputAddress": ["Address:", "300 W Main St"],	
+		"inputAddress2": ["Address2:", ""],	
+		"inputCity": ["City:", "Frankfort"],	
+		"inputState": ["State:", "KY"],	
+		"inputZip": ["Zip:", "40601"],	
+		"inputRating": ["Rating:", "8"],	
+		"inputDate": ["Date:", ""],	
+		"inputArea": [],	
+		"inputCheck": []	
+
+	}
+	
+	
+}
+
+
+
+		item.inputAddress	= ["Address:", document.getElementById("inputAddress").value];
+		item.inputAddress2	= ["Address2:", document.getElementById("inputAddress2").value];
+		item.inputCity		= ["City:", document.getElementById("inputCity").value];
+		item.inputState		= ["State:", document.getElementById("inputState").value];
+		item.inputZip		= ["Zip:", document.getElementById("inputZip").value];
+		item.inputRating	= ["Rating:", document.getElementById("inputRating").value];
+		item.inputDate		= ["Date:", document.getElementById("inputDate").value];
+		//item.inputHidden	= ["Hidden:", $("inputHidden").value];
+		item.inputArea		= ["Area:", document.getElementById("inputArea").value];
+		item.inputCheck		= ["Add to Favorites:", favoriteValue];
